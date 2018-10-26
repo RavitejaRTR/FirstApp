ee7f0539e20fb823dc21b790f66cd817fe87f575
Raviteja Reddy <tejareddy1004@gmail.com>
2018-10-27 02:20:34 +0530

    database connection test

--- src/main/java/com/self/test/PageController.java
+++ src/main/java/com/self/test/PageController.java
@@ -1,5 +1,7 @@
 package com.self.test;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
 import java.util.Optional;
 
 import org.springframework.stereotype.Controller;
@@ -7,10 +9,27 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+
 @Controller
 public class PageController {
+	
+	String url = "jdbc:mysql://localhost:3306/world?useSSL=false";
+	String user = "root";
+	String pswd = "admin";
+	
 	@RequestMapping("/")
 	public String homepage() {
+		
+		try {
+			
+			System.out.println("Trying to connect to COuntry database");
+			Connection myConn = 
+					DriverManager.getConnection(url, user, pswd);
+			System.out.println("Connection successful");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
 		return "index";
 	}
 	
